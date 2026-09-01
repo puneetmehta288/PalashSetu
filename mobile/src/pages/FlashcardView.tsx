cb565de54c09f4c3d28ac2fed72594338877a146
Puneet Mehta <puneetmehta288@gmail.com>
2026-09-01 10:25:14 +0530

    chore: purge dead view stubs (LessonView, WorksheetView, FlashcardView) and clean App routes

--- mobile/src/pages/FlashcardView.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const FlashcardView: React.FC = () => <div><h2>Flashcard View</h2></div>;
-export default FlashcardView;
--- mobile/src/pages/LessonView.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const LessonView: React.FC = () => <div><h2>Lesson View</h2></div>;
-export default LessonView;
